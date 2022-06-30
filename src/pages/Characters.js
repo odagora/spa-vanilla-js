be5365908a61f829315fa942b2454895669a71a9
Daniel <oscar.d.gonzalez@gmail.com>
2022-06-29 21:09:52 -0500

    :sparkles: add home & characters page data from API

--- src/pages/Characters.js
+++ src/pages/Characters.js
@@ -1,17 +1,33 @@
-const Characters = () => {
+import getData from "@utils/getData";
+import getHash from "@utils/getHash";
+
+const Characters = async () => {
+    const id = getHash();
+    const character = await getData(id);
+    const {
+        name,
+        status,
+        species,
+        gender,
+        origin,
+        image,
+        location,
+        episode,
+    } = character;
+
     const view = `
         <div class="characters-inner">
             <article class="characters-card">
-                <img src="image" alt="name">
-                <h2>Name</h2>
+                <img src="${image}" alt="${name}">
+                <h2>${name}</h2>
             </article>
             <article class="characters-card">
-                <h3>Episodes:</h3>
-                <h3>Status:</h3>
-                <h3>Species:</h3>
-                <h3>Gender:</h3>
-                <h3>Origin:</h3>
-                <h3>Last Location:</h3>
+                <h3>Episodes: ${episode.length}</h3>
+                <h3>Status: ${status}</h3>
+                <h3>Species: ${species}</h3>
+                <h3>Gender: ${gender}</h3>
+                <h3>Origin: ${origin.name}</h3>
+                <h3>Last Location: ${location.name}</h3>
             </article>
         </div>
     `;
